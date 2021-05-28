b091b4cf75165ebfc21d04dd03a2b4bb0e41169d
Hugh-Dev <hughpythoneer@gmail.com>
2021-05-27 23:26:02 -0700

    Index Changes

--- assets/views.py
+++ assets/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.generic import View
 
 # Create your views here.
+
+class IndexView(View):
+	"""docstring for IndexView"""
+	template_name = 'template.index.html'
+	def get(self, request):
+		return render(request, self.template_name)
